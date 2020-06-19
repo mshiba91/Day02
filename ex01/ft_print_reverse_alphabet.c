61bb6ec1f5b59475eed85b4cf5b4f0ef7e23202b
Mxolisi Shiba <mshiba@c4r15s5.wethinkcode.co.za>
2020-06-19 14:27:09 +0200

    origin

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+	char alphabet;
+	
+	alphabet= 'Z';
+			while (alphabet >= 'A')
+		{
+			ft_putchar(alphabet);
+			alphabet--;
+		}
+}
+
+int main(void)
+{
+	ft_print_reverse_alphabet();
+	ft_putchar('\n');
+	return (0);
+}
